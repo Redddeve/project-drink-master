c9856df393fe3cd5e4cacf4b3e46dc5be50ab32d
irapanch <117355580+irapanch@users.noreply.github.com>
2023-12-15 23:17:39 +0200

    create FollowUs component

---
+++ src/components/SharedComponents/FollowUs/FollowUs.jsx
@@ -0,0 +1,39 @@
+import { FollowUsWrap } from "./FollowUs.styled";
+
+const FollowUs = () => {
+  return (
+    <FollowUsWrap>
+      <li>
+        <a
+          href="https://www.facebook.com/goITclub/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          F
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.instagram.com/goitclub/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          I
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.youtube.com/c/GoIT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Y
+        </a>
+      </li>
+    </FollowUsWrap>
+  );
+};
+
+export default FollowUs;
