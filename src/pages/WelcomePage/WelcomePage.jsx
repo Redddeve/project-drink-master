f53a4fc70409d46259d65b01015a4fe21adc3604
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-12-15 23:47:32 +0200

    add mob markup

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,13 +1,28 @@
-import { StyledIcon, StyledWrap } from "./WelcomePage.styled";
-import sprite from "../../images/sprite.svg";
+import {
+  StyledBtnSignIn,
+  StyledBtnSignUp,
+  StyledBtnWrap,
+  StyledHead,
+  StyledText,
+  StyledTextWrap,
+  StyledWrap,
+} from "./WelcomePage.styled";
 
 const WelcomePage = () => {
   return (
     <StyledWrap>
-      WelcomePage
-      <StyledIcon width="32" height="32">
-        <use href={sprite + "#icon-logo"} />
-      </StyledIcon>
+      <StyledTextWrap>
+        <StyledHead>Welcome to the app!</StyledHead>
+        <StyledText>
+          This app offers more than just a collection of recipes - it is
+          designed to be your very own digital cookbook. You can easily save and
+          retrieve your own recipes at any time.
+        </StyledText>
+      </StyledTextWrap>
+      <StyledBtnWrap>
+        <StyledBtnSignUp>Sign Up</StyledBtnSignUp>
+        <StyledBtnSignIn>Sign In</StyledBtnSignIn>
+      </StyledBtnWrap>
     </StyledWrap>
   );
 };
