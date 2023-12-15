fbd5bbd0464e76634e6a1b4c46cce4e30ca5a32a
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-12-15 16:05:09 +0200

    fix sprite
    testing Ok

---
+++ src/pages/WelcomePage/WelcomePage.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledIcon = styled.svg`
+  cursor: pointer;
+  fill: black;
+  stroke: black;
+
+  /* color: black; */
+`;
+
+export const StyledWrap = styled.div`
+  background-color: grey;
+`;
