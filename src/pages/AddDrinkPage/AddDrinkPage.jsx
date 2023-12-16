4b75a6a4ccdadc26c9784490497785b247b8205e
Kateryna <ikatya7575@gmaiil.com>
2023-12-16 22:50:15 +0300

    created html markuo of form, added styled components, implemented mobile. tablet and desktop version of interface, added logic of geting form data
    it remains to add validation of input fields, POST request to the server  and the logic of displaying a cocktail image

--- src/pages/AddDrinkPage/AddDrinkPage.jsx
+++ src/pages/AddDrinkPage/AddDrinkPage.jsx
@@ -1,5 +1,267 @@
+import { useState } from "react";
+import Select from "react-select";
+import { Controller, useForm } from "react-hook-form";
+import deleteSvg from "./delete.svg";
+import {
+  StyledAddBtn,
+  StyledAddDiv,
+  StyledAddTitle,
+  StyledAfterContent,
+  StyledDescInput,
+  StyledFileInput,
+  StyledFileLabel,
+  StyledInfoDiv,
+  StyledIngDiv,
+  StyledIngFieldBtn,
+  StyledIngFieldInput,
+  StyledIngFieldLabel,
+  StyledIngFieldWrapper,
+  StyledIngTitle,
+  StyledIngTitleDiv,
+  StyledRadioLabel,
+  StyledRadioLabelDiv,
+  StyledSelectLabel,
+  StyledSubmitBtn,
+  StyledTitleInput,
+  stylesSelect,
+  ingStyles,
+} from "./AddDrinkPage.styled";
+import { categoryOptions, glassOptions, ingOptions } from "./options";
+
 const AddDrinkPage = () => {
-  return <div>AddDrinkPage</div>;
+  const [ingNumber, setIngNumber] = useState([1, 2, 3]);
+  const [category, setCategory] = useState("Cocktail");
+  const [glass, setGlass] = useState("Highball glass");
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [glassMenuIsOpen, setGlassMenuIsOpen] = useState(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [formData, setFormData] = useState({
+    Ingredients: [],
+    aboutRecipe: "",
+    itemTitle: "",
+    photo: "",
+    category: "",
+    glass: "",
+    recipeDesc: "",
+    alcohol: false,
+  });
+
+  // ****   STYLES FOR SELECT  ****
+  const styles = {
+    ...stylesSelect,
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      transition: "transform 0.2s ease",
+      transform: menuIsOpen ? "rotate(180deg)" : null,
+    }),
+  };
+  const glassStyles = {
+    ...stylesSelect,
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      transition: "transform 0.2s ease",
+      transform: glassMenuIsOpen ? "rotate(180deg)" : null,
+    }),
+  };
+
+  // ****   STYLES FOR SELECT  ****
+
+  const addIngField = (e) => {
+    e.preventDefault();
+    setIngNumber([...ingNumber, ingNumber.length + 1]);
+  };
+
+  const deleteIngField = (e) => {
+    e.preventDefault();
+    const newNumber = ingNumber.slice(0, ingNumber.length - 1);
+    setIngNumber(newNumber);
+  };
+
+  const deleteIng = (e, index) => {
+    e.preventDefault();
+
+    const newNumbers = formData.Ingredients.filter((_, i) => i !== index);
+    setFormData({ ...formData, Ingredients: newNumbers });
+  };
+
+  const onSubmit = (data) => {
+    const ingredientsArray = ingNumber.map((el, index) => ({
+      [`Ingredients`]: data[`Ingredients${index}`].value,
+      [`IngNumber`]: data[`IngNumber${index}`],
+    }));
+
+    setFormData({
+      ...data,
+      Ingredients: ingredientsArray,
+      glass: glass,
+      category: category,
+    });
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <StyledAddTitle>Add drink</StyledAddTitle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <StyledInfoDiv>
+          <StyledFileLabel>
+            <StyledFileInput
+              type="file"
+              {...register("photo")}
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </StyledFileLabel>
+          <div>
+            <label>
+              <StyledTitleInput
+                type="text"
+                {...register("itemTitle", { required: true })}
+                placeholder="Enter item title"
+              />
+              {errors.itemTitle && <p>Item title is required.</p>}
+            </label>
+            <label>
+              <StyledTitleInput
+                type="text"
+                {...register("aboutRecipe", { required: true })}
+                placeholder="Enter about recipe"
+              />
+              {errors.aboutRecipe && <p>About recipe field is required.</p>}
+            </label>
+            <StyledSelectLabel>
+              <p>Category</p>
+              <Select
+                styles={styles}
+                options={categoryOptions}
+                value={{ label: category, value: category }}
+                onChange={(val) => setCategory(val.value)}
+                theme={(theme) => ({
+                  ...theme,
+                  colors: {
+                    neutral50: "#fff",
+                  },
+                })}
+                menuIsOpen={menuIsOpen}
+                onMenuOpen={() => setMenuIsOpen(true)}
+                onMenuClose={() => setMenuIsOpen(false)}
+              />
+            </StyledSelectLabel>
+            <StyledSelectLabel>
+              <p>Glass</p>
+              <Select
+                styles={glassStyles}
+                options={glassOptions}
+                value={{ label: glass, value: glass }}
+                onChange={(val) => setGlass(val.value)}
+                theme={(theme) => ({
+                  ...theme,
+                  colors: {
+                    neutral50: "#fff",
+                  },
+                })}
+                menuIsOpen={glassMenuIsOpen}
+                onMenuOpen={() => setGlassMenuIsOpen(true)}
+                onMenuClose={() => setGlassMenuIsOpen(false)}
+              />
+            </StyledSelectLabel>
+            <StyledRadioLabelDiv>
+              <StyledRadioLabel>
+                <input
+                  type="radio"
+                  value={false}
+                  name="alcohol"
+                  {...register("alcohol", { required: true })}
+                />
+                <p>No Alcohol</p>
+              </StyledRadioLabel>
+              <StyledRadioLabel>
+                <input
+                  type="radio"
+                  name="alcohol"
+                  value={true}
+                  {...register("alcohol", { required: true })}
+                />
+                <p>Alcohol</p>
+              </StyledRadioLabel>
+            </StyledRadioLabelDiv>
+            {errors.alcohol && <p>At least one option must be selected.</p>}
+          </div>
+        </StyledInfoDiv>
+        <StyledIngDiv>
+          <StyledIngTitleDiv>
+            <StyledIngTitle>Ingredients</StyledIngTitle>
+            <StyledAddDiv>
+              <StyledAddBtn
+                onClick={(e) => {
+                  deleteIngField(e);
+                }}
+                disabled={ingNumber.length <= 3}
+              >
+                -
+              </StyledAddBtn>
+              <p>{ingNumber.length}</p>
+              <StyledAddBtn onClick={(e) => addIngField(e)}>+</StyledAddBtn>
+            </StyledAddDiv>
+          </StyledIngTitleDiv>
+
+          {ingNumber.map((el, index) => {
+            return (
+              <StyledIngFieldLabel key={el}>
+                <label>
+                  <Controller
+                    name={`Ingredients${index}`}
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        styles={ingStyles}
+                        {...field}
+                        options={ingOptions}
+                        theme={(theme) => ({
+                          ...theme,
+                          colors: {
+                            neutral50: "rgba(243, 243, 243, 0.8)",
+                          },
+                        })}
+                      />
+                    )}
+                    rules={{ required: true }}
+                  />
+                </label>
+
+                <StyledIngFieldWrapper>
+                  <StyledIngFieldInput
+                    type="number"
+                    {...register(`IngNumber${index}`, {
+                      maxLength: 3,
+                      pattern: /^\d{0,3}$/,
+                    })}
+                  />
+                  <StyledAfterContent>cl</StyledAfterContent>
+                </StyledIngFieldWrapper>
+                <StyledIngFieldBtn onClick={(e) => deleteIng(e, index)}>
+                  <img src={deleteSvg} alt="" />
+                </StyledIngFieldBtn>
+              </StyledIngFieldLabel>
+            );
+          })}
+        </StyledIngDiv>
+        <label>
+          <StyledIngTitle>Recipe Preparation</StyledIngTitle>
+          <StyledDescInput
+            type="text"
+            {...register("recipeDesc", { required: true })}
+            placeholder="Enter the recipe"
+          />
+        </label>
+        <StyledSubmitBtn type="submit">Add</StyledSubmitBtn>
+      </form>
+    </div>
+  );
 };
 
 export default AddDrinkPage;
---
+++ src/pages/AddDrinkPage/AddDrinkPage.styled.js
@@ -0,0 +1,359 @@
+import styled from "styled-components";
+import { device } from "../../styles/device";
+
+export const StyledAddTitle = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 38px;
+  margin-bottom: 40px;
+`;
+
+export const StyledFileLabel = styled.label`
+  width: 335px;
+  height: 320px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  background: rgba(22, 31, 55, 0.5);
+  margin-bottom: 40px;
+  &::before {
+    content: "+";
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    border-radius: 6px;
+    background: #f3f3f3;
+    font-size: 45px;
+    color: black;
+    padding: 11px;
+    margin-bottom: 18px;
+  }
+  &::after {
+    content: "Add image";
+    color: #f3f3f3;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 20px; /* 125% */
+  }
+  @media ${device.tablet} {
+    min-width: 320px;
+    margin-bottom: 0;
+  }
+  @media only screen and (min-width: 1440px) {
+    min-width: 400px;
+    gap: 40px;
+  }
+`;
+export const StyledFileInput = styled.input`
+  display: none;
+`;
+export const StyledTitleInput = styled.input`
+  width: 335px;
+  height: 35px;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #f3f3f3;
+  padding-bottom: 14px;
+  margin-bottom: 31px;
+  color: #f3f3f3;
+  @media ${device.tablet} {
+    padding-bottom: 18px;
+    width: 352px;
+  }
+`;
+export const StyledSelectLabel = styled.label`
+  width: 335px;
+  height: 35px;
+  align-items: center;
+  padding-bottom: 14px;
+  display: flex;
+  margin-bottom: 31px;
+  justify-content: space-between;
+  border-bottom: 1px solid #f3f3f3;
+  color: rgba(243, 243, 243, 0.5);
+  padding-left: 3px;
+  @media ${device.tablet} {
+    padding-bottom: 18px;
+    width: 352px;
+  }
+`;
+
+export const StyledRadioLabel = styled.label`
+  color: rgba(243, 243, 243, 0.5);
+  display: flex;
+  margin-bottom: 80px;
+  input[type="radio"]:checked + p {
+    color: rgba(243, 243, 243, 1);
+    &::before {
+      content: "";
+      background-color: white;
+      border: 2px solid rgba(243, 243, 243, 0.5);
+    }
+    &::after {
+      content: "";
+      display: flex;
+      border-radius: 50%;
+      top: 2.2px;
+      left: 2.2px;
+      border: 3.1px solid black;
+      width: 12px;
+      height: 12px;
+      opacity: 1;
+      position: absolute;
+      z-index: 3;
+    }
+  }
+  position: relative;
+
+  input[type="radio"] {
+    opacity: 0;
+  }
+  input[type="radio"] + p {
+    padding-left: 15px;
+    &::before {
+      content: "";
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+      border: unset;
+      border: 3px solid rgba(243, 243, 243, 0.5);
+      width: 17px;
+      height: 17px;
+      opacity: 1;
+      position: absolute;
+      z-index: 2;
+      top: 0;
+      left: 0;
+    }
+  }
+`;
+export const StyledRadioLabelDiv = styled.label`
+  display: flex;
+  gap: 30px;
+  @media ${device.tablet} {
+    padding-top: 30px;
+    max-height: 32px;
+  }
+`;
+
+export const StyledIngTitle = styled.p`
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 32px;
+`;
+export const StyledIngTitleDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StyledAddDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 104px;
+  border-radius: 200px;
+  border: 1px solid rgba(243, 243, 243, 0.5);
+  height: 38px;
+  margin-bottom: 34px;
+`;
+export const StyledAddBtn = styled.button`
+  background-color: transparent;
+  border: none;
+  width: 16px;
+  height: 16px;
+  font-size: 30px;
+  line-height: 0;
+  padding: 0;
+  justify-content: center;
+  align-items: center;
+  color: inherit;
+  font-family: inherit;
+  &:disabled {
+    color: rgba(243, 243, 243, 0.5);
+  }
+`;
+export const StyledIngFieldInput = styled.input`
+  width: 101px;
+  height: 50px;
+  border-radius: 200px;
+  border: 1px solid rgba(243, 243, 243, 0.5);
+  background: transparent;
+  color: rgba(243, 243, 243, 1);
+  padding: 0 60px 0 18px;
+  position: relative;
+  &::after {
+    content: "cl";
+    display: block;
+    width: 10px;
+    height: 10px;
+    color: white;
+    right: 30px;
+    top: 10px;
+  }
+  @media ${device.tablet} {
+    width: 150px;
+  }
+`;
+export const StyledIngFieldLabel = styled.label`
+  display: flex;
+  margin-bottom: 18px;
+  gap: 8px;
+`;
+export const StyledIngFieldWrapper = styled.div`
+  position: relative;
+`;
+export const StyledAfterContent = styled.span`
+  content: "cl";
+  display: block;
+  width: 10px;
+  height: 10px;
+  color: white;
+  position: absolute;
+  right: 50px;
+  top: 20px;
+  transform: translateY(-50%);
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+`;
+export const StyledIngFieldBtn = styled.button`
+  background: transparent;
+  border: none;
+  padding: 0;
+  @media ${device.tablet} {
+    margin-left: 188px;
+  }
+  @media only screen and (min-width: 1440px) {
+    margin-left: 20px;
+  }
+`;
+export const StyledIngDiv = styled.div`
+  padding-bottom: 80px;
+  @media ${device.tablet} {
+    max-width: 704px;
+  }
+  @media only screen and (min-width: 1440px) {
+    max-width: 540px;
+  }
+`;
+export const StyledInfoDiv = styled.div`
+  @media ${device.tablet} {
+    display: flex;
+    gap: 32px;
+    padding-bottom: 80px;
+    max-width: 833px;
+  }
+`;
+export const StyledDescInput = styled.textarea`
+  width: 335px;
+  min-height: 184px;
+  margin-top: 40px;
+  padding: 16px;
+  margin-bottom: 20px;
+  background: transparent;
+  border-radius: 14px;
+  border: 1px solid rgba(243, 243, 243, 0.5);
+  color: rgba(243, 243, 243, 1);
+  line-height: 1;
+  vertical-align: top;
+  @media ${device.tablet} {
+    width: 480px;
+  }
+`;
+
+export const StyledSubmitBtn = styled.button`
+  border-radius: 42px;
+  background: #f3f3f3;
+  display: inline-flex;
+  padding: 14px 40px;
+  align-items: flex-start;
+  gap: 10px;
+  color: #161f37;
+
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 18px;
+  @media ${device.tablet} {
+    display: block;
+  }
+`;
+export const stylesSelect = {
+  control: (baseStyles, state) => ({
+    ...baseStyles,
+    width: 200,
+    height: 50,
+    color: "rgba(243, 243, 243, 1)",
+    border: "none",
+    textAlign: "right",
+    background: "transparent",
+  }),
+  option: (styles, state) => ({
+    ...styles,
+    padding: "3px 10px",
+    fontSize: "12px",
+    backgroundColor: state.isSelected ? "rgba(255, 255, 255, 0.10)" : "",
+    color: state.isSelected ? "#F3F3F3" : "#F3F3F366",
+    ":hover": {
+      background: "rgba(255, 255, 255, 0.10)",
+      color: "#F3F3F3",
+    },
+  }),
+  menu: (baseStyles) => ({
+    ...baseStyles,
+    background: "#161F37",
+    border: "none ",
+    borderRadius: "12px ",
+    maxHeight: "256px",
+    width: "131px",
+    right: "0",
+  }),
+  //   menuList: (base) => ({
+  //     ...base,
+  //     "::-webkit-scrollbar": {
+  //       width: "2px",
+  //       height: "0px",
+  //     },
+  //     "::-webkit-scrollbar-track": {
+  //       background: "#eaeaea",
+  //     },
+  //     "::-webkit-scrollbar-thumb": {
+  //       background: "#2f2c54",
+  //     },
+  //     "::-webkit-scrollbar-thumb:hover": {
+  //       background: "#0a0342",
+  //     },
+  //   }),
+};
+export const ingStyles = {
+  ...stylesSelect,
+  control: (baseStyles, state) => ({
+    ...baseStyles,
+    width: 200,
+    height: 50,
+    color: "rgba(243, 243, 243, 1)",
+    border: "border: 1px solid rgba(243, 243, 243, 0.50)",
+    borderRadius: "200px",
+    textAlign: "left",
+    background: "transparent",
+    "@media only screen and (min-width:768px)": {
+      ...stylesSelect["@media only screen and (min-width: 768px)"],
+      width: 332,
+    },
+  }),
+  menu: (baseStyles) => ({
+    ...baseStyles,
+    background: "#161F37",
+    border: "none ",
+    borderRadius: "12px ",
+    maxHeight: "256px",
+    width: "100%",
+    left: "0",
+  }),
+};
