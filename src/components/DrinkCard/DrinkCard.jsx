fcd46e209728319e2c047cfad7c9457ec1165cf9
Disclo5ure <46845468m@gmail.com>
2023-12-17 00:52:06 +0200

    create drink card component

---
+++ src/components/DrinkCard/DrinkCard.jsx
@@ -0,0 +1,39 @@
+import {
+  StyledDrinkImage,
+  StyledDrinkCard,
+  StyledTextContainerSmall,
+  StyledSeeMoreSmall,
+} from "./DrinkCard.styled";
+import PropTypes from "prop-types";
+
+export const DrinkCard = ({ drink, detailed }) => {
+  return (
+    <StyledDrinkCard>
+      <StyledDrinkImage
+        src={drink.drinkThumb}
+        alt={drink.drink}
+      ></StyledDrinkImage>
+      {!detailed ? (
+        <StyledTextContainerSmall>
+          <span>{drink.drink}</span>
+          <StyledSeeMoreSmall to={`../drink/${drink._id.$oid}`}>
+            See more
+          </StyledSeeMoreSmall>
+        </StyledTextContainerSmall>
+      ) : (
+        <></>
+      )}
+    </StyledDrinkCard>
+  );
+};
+
+DrinkCard.propTypes = {
+  drink: PropTypes.shape({
+    drinkThumb: PropTypes.string,
+    drink: PropTypes.string,
+    _id: PropTypes.shape({
+      $oid: PropTypes.string,
+    }),
+  }),
+  detailed: PropTypes.bool,
+};
