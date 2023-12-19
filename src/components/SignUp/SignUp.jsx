8b293cd53e0f51dba814303db043b1635ce3ea07
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-12-19 20:47:48 +0200

    complitte with errors

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useDispatch } from 'react-redux';
 
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
+// import { yupResolver } from '@hookform/resolvers/yup';
 
 import {
   StyledWrap,
@@ -17,6 +17,8 @@ import {
   StyledFormWrap,
   StyledDatePicker,
   StyledCalendarIcon,
+  StyledInputWrap,
+  StyledMessage,
 } from './SignUp.styled.js';
 import sprite from '../../images/sprite.svg';
 
@@ -75,45 +77,105 @@ const SignUp = () => {
     <StyledWrap>
       <StyledFormWrap onSubmit={handleSubmit(submit)}>
         <StyledHead>Sign Up</StyledHead>
-        <StyledInput
-          {...register('name', {
-            required: "name can't be empty",
-            minLength: {
-              value: 2,
-              message: 'name must contain at least 2 characters',
-            },
-          })}
-          placeholder="Name"
-          className={errors?.name ? 'error' : isValid ? 'correct' : ''}
-          // style={
-          //   errors?.name
-          //     ? { border: '1px solid red' }
-          //     : isValid
-          //     ? { border: '1px solid green' }
-          //     : { border: '1px solid white' }
-          // }
-        />
-        {errors?.name && <p>{errors?.name?.message || 'This is an ERROR'}</p>}
-        {!errors?.name && isValid && <p>ok</p>}
+        <StyledInputWrap>
+          <StyledInput
+            {...register('name', {
+              required: "name can't be empty",
+              minLength: {
+                value: 2,
+                message: 'name must contain at least 2 characters',
+              },
+            })}
+            placeholder="Name"
+            className={errors?.name ? 'error' : isValid ? 'correct' : ''}
+            // $content={
+            //   errors?.name ? errors?.name?.message : isValid ? 'ok' : ''
+            // }
+          />
+          {errors?.name && (
+            <StyledMessage className="error">
+              {errors?.name?.message || 'ERROR'}
+            </StyledMessage>
+          )}
+          {!errors?.name && isValid && (
+            <StyledMessage className="correct">ok</StyledMessage>
+          )}
+        </StyledInputWrap>
 
         {/* <StyledInput {...register('date')} placeholder="dd/mm/yyyy" /> */}
-
-        <StyledDatePicker
-          {...register('date')}
-          showIcon
-          selected={date}
-          onChange={date => setDate(date)}
-          icon={
-            <StyledCalendarIcon>
-              <use href={`${sprite}#icon-calendar`} />
-            </StyledCalendarIcon>
-          }
-          placeholderText="dd/mm/yyyy"
-        />
-
-        <StyledInput {...register('email')} placeholder="Email" />
-        <StyledInput {...register('password')} placeholder="Password" />
-        <StyledSignInBtn>Sign Up</StyledSignInBtn>
+        <StyledInputWrap>
+          <StyledDatePicker
+            {...register('date', {
+              required: "date can't be empty",
+            })}
+            showIcon
+            selected={date}
+            onChange={date => setDate(date)}
+            icon={
+              <StyledCalendarIcon>
+                <use href={`${sprite}#icon-calendar`} />
+              </StyledCalendarIcon>
+            }
+            placeholderText="dd/mm/yyyy"
+            className={errors?.date ? 'error' : isValid ? 'correct' : ''}
+          />
+          {errors?.date && (
+            <StyledMessage className="error">
+              {errors?.date?.message || 'ERROR'}
+            </StyledMessage>
+          )}
+          {!errors?.date && isValid && (
+            <StyledMessage className="correct">ok</StyledMessage>
+          )}
+        </StyledInputWrap>
+        <StyledInputWrap>
+          <StyledInput
+            {...register('email', {
+              required: "email can't be empty",
+              minLength: {
+                value: 5,
+                message: 'email must contain at least 5 characters',
+              },
+              pattern: {
+                value:
+                  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
+                message: 'Invalid characters or missing  @ or . ',
+              },
+            })}
+            placeholder="Email"
+            className={errors?.email ? 'error' : isValid ? 'correct' : ''}
+          />
+          {errors?.email && (
+            <StyledMessage className="error">
+              {errors?.email?.message || 'ERROR'}
+            </StyledMessage>
+          )}
+          {!errors?.email && isValid && (
+            <StyledMessage className="correct">ok</StyledMessage>
+          )}
+        </StyledInputWrap>
+        <StyledInputWrap>
+          <StyledInput
+            {...register('password', {
+              required: "password can't be empty",
+              minLength: {
+                value: 6,
+                message: 'password must contain at least 6 characters',
+              },
+            })}
+            placeholder="Password"
+            className={errors?.password ? 'error' : isValid ? 'correct' : ''}
+          />
+          {errors?.password && (
+            <StyledMessage className="error">
+              {errors?.password?.message || 'ERROR'}
+            </StyledMessage>
+          )}
+          {!errors?.password && isValid && (
+            <StyledMessage className="correct">ok</StyledMessage>
+          )}
+        </StyledInputWrap>
+        <StyledSignInBtn disabled={!isValid}>Sign Up</StyledSignInBtn>
 
         <StyledLink to="/signin">Sign In</StyledLink>
       </StyledFormWrap>
