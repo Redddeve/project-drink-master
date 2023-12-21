78091faaf1af1eb4f3712adabfba548103864644
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-12-21 14:40:11 +0200

    add welcome styles

---
+++ src/pages/Auth/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import SignUp from '../../../components/SignUp/SignUp.jsx';
+import { StyledPageWrap } from '../AuthPages.styled';
+
+const SignUpPage = () => {
+  return (
+    <StyledPageWrap>
+      <SignUp />
+    </StyledPageWrap>
+  );
+};
+
+export default SignUpPage;
