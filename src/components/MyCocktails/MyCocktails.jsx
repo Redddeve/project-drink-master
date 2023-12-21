0cae78f1bb162164eca3c30eea68687e239fd6d4
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-12-21 16:32:17 +0200

    myCoctails fix
    Сделал myCoctails как по тех заданию, убрал лишние компоненты и стили.

--- src/components/MyCocktails/MyCocktails.jsx
+++ src/components/MyCocktails/MyCocktails.jsx
@@ -1,126 +1,39 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CocktailList from '../CocktailList/CocktailList';
-import {
-  selectAllDrinks,
-  selectIngredients,
-  selectOwnDrinks,
-} from '../../redux/drinks/selectors';
-import {
-  fetchAllDrinks,
-  getIngredientsThunk,
-} from '../../redux/drinks/operations';
-import {
-  EmptyMyCocktailsContainer,
-  CocktailTitle,
-  SemiDescription,
-  Description,
-  CocktailImg,
-  IngredientsTitle,
-  IngredientsGrid,
-  GridItem,
-  ImgBox,
-  IngredientBox,
-  IngredientMeasure,
-  IngredientTitle,
-  RecipeTitle,
-  DescBox,
-  DescriptionText,
-  MainImg,
-  CocktailImgDiv,
-  HeroBlock,
-  RecipeBox,
-} from './MyCocktails.styled';
-import CommonBtn from '../../components/SharedComponents/CommonBtn/CommonBtn';
-import mobile from '../../images/three-cocktails-mobile@1x.jpg';
-import usual from '../../images/three-cocktails@1x.jpg';
-import retina from '../../images/three-cocktails@2x.jpg';
-import Switcher from '../../pages/MyDrinksPage/imageSwitcher';
+import { selectOwnDrinks } from '../../redux/drinks/selectors';
+import { getOwnDrinksThunk } from '../../redux/drinks/operations';
+import usual from '../../images/blue-iced-tea@1x.png';
+import retina from '../../images/blue-iced-tea@2x.png';
 import PaginatedItems from '../Paginator/Paginator';
+import {
+  EmptyDescription,
+  EmptyFavoritesContainer,
+  EmptyFavoritesImage,
+} from '../FavoriteCocktails/FavoriteCocktails.styled';
 
 const MyOwnCocktails = () => {
   const ownCocktailsList = useSelector(selectOwnDrinks);
-  const ingredientList = useSelector(selectIngredients);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchAllDrinks());
-    dispatch(getIngredientsThunk());
+    dispatch(getOwnDrinksThunk());
   }, [dispatch]);
 
-  const allDrinks = useSelector(selectAllDrinks);
-  const testCocktailId = '639b6de9ff77d221f190c563';
-  const oneDrink = allDrinks?.Shake?.find(item => item._id === testCocktailId);
-
-  const ingredientsIdArray = oneDrink?.ingredients?.map(
-    item => item.ingredientId
-  );
-  const filteredObjects = ingredientList?.filter(obj =>
-    ingredientsIdArray?.includes(obj._id)
-  );
   return ownCocktailsList.length === 0 ? (
-    <EmptyMyCocktailsContainer>
-      {oneDrink ? (
-        <>
-          <HeroBlock>
-            <div>
-              <CocktailTitle>{oneDrink.drink}</CocktailTitle>
-              <SemiDescription>
-                {oneDrink.glass}/{oneDrink.alcoholic}
-              </SemiDescription>
-              <Description>{oneDrink.description}</Description>
-              <CommonBtn type="submit" variant="addFavorite">
-                Add to favorite drinks
-              </CommonBtn>
-            </div>
-            <div>
-              <CocktailImgDiv></CocktailImgDiv>
-              <CocktailImg src={oneDrink.drinkThumb} alt="cocktailImg" />
-            </div>
-          </HeroBlock>
-          <IngredientsTitle>Ingredients</IngredientsTitle>
-          <IngredientsGrid>
-            {filteredObjects?.map(item => (
-              <GridItem key={item._id}>
-                <ImgBox>
-                  <MainImg
-                    srcSet={(item.thumbSmall, item.thumbMedium)}
-                    src={item.thumbSmall}
-                    alt="thumbSmall"
-                  />
-                </ImgBox>
-                <IngredientBox>
-                  <IngredientTitle>{item.title}</IngredientTitle>
-                  <IngredientMeasure>
-                    {
-                      oneDrink?.ingredients?.find(
-                        obj => obj.ingredientId === item._id
-                      )?.measure
-                    }
-                  </IngredientMeasure>
-                </IngredientBox>
-              </GridItem>
-            ))}
-          </IngredientsGrid>
-          <RecipeTitle>Recipe Preparation</RecipeTitle>
-          <RecipeBox>
-            <DescBox>
-              <DescriptionText>{oneDrink.description}</DescriptionText>
-              <DescriptionText>{oneDrink.instructions}</DescriptionText>
-            </DescBox>
-            <div>
-              <Switcher mobile={mobile} usual={usual} retina={retina} />
-            </div>
-          </RecipeBox>
-        </>
-      ) : (
-        <p>Oops!!!</p>
-      )}
-    </EmptyMyCocktailsContainer>
+    <EmptyFavoritesContainer>
+      <EmptyFavoritesImage
+        srcSet={`${usual} 1x, ${retina} 2x`}
+        alt="Cocktail"
+      />
+      <EmptyDescription>
+        You haven&rsquo;t added any cocktails yet
+      </EmptyDescription>
+    </EmptyFavoritesContainer>
   ) : (
     <PaginatedItems
       items={ownCocktailsList}
-      destination="favorite"
+      destination="my"
       ListComponent={CocktailList}
       itemsPerPageValue={{ mobile: 3, tablet: 3, desktop: 3, default: 9 }}
     />
--- src/pages/MyDrinksPage/MyDrinksPage.jsx
+++ src/pages/MyDrinksPage/MyDrinksPage.jsx
@@ -1,15 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import MyOwnCocktails from '../../components/MyCocktails/MyCocktails';
 import { useEffect } from 'react';
 import { getOwnDrinksThunk } from '../../redux/drinks/operations';
-import { MyCocktailsTitle } from './MyDrinksPage.styled';
 import PageTitle from '../../components/PageTitle/PageTitle';
-import { selectOwnDrinks } from '../../redux/drinks/selectors';
 import ButtonUpToTop from '../../components/ButtonUpToTop/ButtonUpToTop';
 
 const MyDrinksPage = () => {
-  const ownCocktailsList = useSelector(selectOwnDrinks);
-
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getOwnDrinksThunk());
@@ -17,10 +13,7 @@ const MyDrinksPage = () => {
 
   return (
     <section>
-      {ownCocktailsList.length !== 0 ? (
-        <PageTitle title={'My drinks'}></PageTitle>
-      ) : null}
-      <MyCocktailsTitle></MyCocktailsTitle>
+      <PageTitle title={'My drinks'}></PageTitle>
       <MyOwnCocktails />
       <ButtonUpToTop />
     </section>
