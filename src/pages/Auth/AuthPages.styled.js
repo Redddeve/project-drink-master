f1e0707c45de9e4c9a94ab8a296cd6ed007eff7a
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-12-23 20:18:20 +0200

    add bg styles
    I went through all the options. This one is the most normal

--- src/pages/Auth/AuthPages.styled.js
+++ src/pages/Auth/AuthPages.styled.js
@@ -1,27 +1,40 @@
 import styled from 'styled-components';
 
-import bgMob from '../../images/start-bg-mob@2x.jpg';
-import bgTab from '../../images/start-bg-tab@2x.jpg';
-import bgDesc from '../../images/start-bg-desc@2x.jpg';
-
+import bgMobFull from '../../images/start-bg-mob@2x.jpg';
+import bgTabFull from '../../images/start-bg-tab@2x.jpg';
+import bgDeskFull from '../../images/start-bg-desc@2x.jpg';
 import { device } from '../../styles/device';
+
+export const StyledBgWrap = styled.div`
+  background-image: url(${bgMobFull});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  max-width: 100vw;
+  height: 100vh;
+
+  @media ${device.tablet} {
+    background-image: url(${bgTabFull});
+  }
+
+  @media ${device.desktop} {
+    background-image: url(${bgDeskFull});
+  }
+`;
 
 export const StyledPageWrap = styled.div`
   height: 100vh;
   min-width: 335px;
   max-width: 375px;
-  background-image: url(${bgMob});
-  background-repeat: no-repeat;
-  background-size: cover;
+  z-index: 100;
   margin: 0 auto;
 
   @media ${device.tablet} {
     max-width: 768px;
-    background-image: url(${bgTab});
   }
 
   @media ${device.desktop} {
     max-width: 1440px;
-    background-image: url(${bgDesc});
   }
 `;
