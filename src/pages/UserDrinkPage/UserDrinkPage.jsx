dc2916d7352961955a5020e0e6884714e67e9951
DianaKryzhanivska <di_parkhomenko@ukr.net>
2023-12-25 19:40:36 +0200

    motivational modal of adding first & tenth fav drink

--- src/pages/UserDrinkPage/UserDrinkPage.jsx
+++ src/pages/UserDrinkPage/UserDrinkPage.jsx
@@ -22,8 +22,26 @@ import {
 import DrinkIngredientsList from '../../components/DrinkIngredientsList/DrinkIngredientsList.jsx';
 import RecipePreparation from '../../components/RecipePreparation/RecipePreparation.jsx';
 import { selectTheme } from '../../redux/theme/selectors.js';
+import Modal from '../../components/Modal/Modal.jsx';
+import { useMediaQuery } from 'react-responsive';
+import firstFavMob from '../../images/motivation/Motivation-mob3@2x.jpg';
+import firstFavTabDesc from '../../images/motivation/motivation3@2x.jpg';
+import tenthFavMob from '../../images/motivation/Motivation-mob2@2x.jpg';
+import tenthFavTabDesc from '../../images/motivation/motivation2@2x.jpg';
 
 const UserDrinkPage = () => {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 767px)',
+  });
+  const [background, setBackground] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   const theme = useSelector(selectTheme);
   const { drinkId } = useParams();
   const dispatch = useDispatch();
@@ -33,14 +51,36 @@ const UserDrinkPage = () => {
   const [isFavorite, setIsFavorite] = useState(false);
 
   const handleAddToFav = () => {
-    dispatch(addFavoriteDrinkThunk(drinkId));
+    dispatch(addFavoriteDrinkThunk(drinkId))
+      .then(data => {
+        const { sendFirstFavGreeting, sendTenthFavGreeting } = data.payload;
+
+        const firstFav = sendFirstFavGreeting === true;
+        const tenthFav = sendTenthFavGreeting === true;
+
+        if (firstFav) {
+          openModal();
+          setModalMessage(
+            'Wow! You have added the first recipe to your favorites!'
+          );
+          setBackground(isMobile ? firstFavMob : firstFavTabDesc);
+        }
+        if (tenthFav) {
+          openModal();
+          setModalMessage('Wow! You have added 10 recipes to your favorites!');
+          setBackground(isMobile ? tenthFavMob : tenthFavTabDesc);
+        }
+      })
+      .catch(error => {
+        console.error('Error in addFavoriteDrinkThunk:', error);
+      });
   };
   const handleRemoveFromFav = () => {
     dispatch(removeFavoriteDrinkThunk(drinkId));
   };
 
   useEffect(() => {
-    dispatch(getFavoriteDrinksThunk({page: 1, itemsPerPage: 1000}));
+    dispatch(getFavoriteDrinksThunk({ page: 1, itemsPerPage: 1000 }));
     dispatch(getDrinkbyIdThunk(drinkId));
     window.scrollTo({
       top: 0,
@@ -76,6 +116,16 @@ const UserDrinkPage = () => {
         ingredientsArray={drink ? drink.ingredients : []}
       />
       <RecipePreparation theme={theme} instructions={drink.instructions} />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => {
+          closeModal();
+          setModalMessage('');
+          setBackground('');
+        }}
+        message={modalMessage}
+        background={background}
+      />
     </>
   );
 };
