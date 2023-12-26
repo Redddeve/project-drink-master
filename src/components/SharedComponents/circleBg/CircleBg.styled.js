ed0889199ee5c4870d7b751a3503e3111dd9eeb4
irapanch <117355580+irapanch@users.noreply.github.com>
2023-12-26 15:22:56 +0200

    create circleBg component

---
+++ src/components/SharedComponents/circleBg/CircleBg.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { device } from '../../../styles/device';
+
+export const Circle = styled.div`
+  display: none;
+
+  @media ${device.tablet} {
+    position: absolute;
+    display: block;
+    top: 465px;
+    right: -425px;
+    width: 549px;
+    height: 543px;
+    border-radius: 549px;
+
+    background: ${({ theme }) =>
+      theme === 'dark' ? 'rgba(64, 112, 205, 0.5)' : 'rgba(64, 112, 205, 0.1)'};
+    filter: blur(104.8543701171875px);
+  }
+  @media ${device.desktop} {
+    top: 425px;
+    right: -300px;
+  }
+`;
